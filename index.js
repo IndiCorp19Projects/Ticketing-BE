23666288b6a0aa256f2fd5f2544771ba07a3e219
Deepanshu Sharma <deepanshu.sharma2@indicorpit.co.in>
2025-11-07 17:15:16 +0530

    updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+require("./slaMonitor");
 
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
